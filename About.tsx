580f7786a3d3e5c02cdc212fc8cfe2ca80aaacbf
Muyio <108817305+Muyio@users.noreply.github.com>
2022-08-16 20:47:34 +0100

    Add files via upload

---
+++ About.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import Navbar from "../Comp/Navbar";
+
+const About = () => {
+    return (
+                <div className= "Check">
+                    <h1> Always loof</h1>
+                    <Navbar />
+                </div>
+            
+    );
+}
+export default About;
