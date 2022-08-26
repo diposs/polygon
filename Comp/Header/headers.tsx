535c3182a034be568ec1284c8366df69dbc7f6f9
diposs <onakunle.dipo@gmail.com>
2022-08-26 11:08:38 +0100

    modified:   Comp/Header/header.styles.ts 	modified:   Comp/Header/headers.tsx 	modified:   tsconfig.json

--- Comp/Header/headers.tsx
+++ Comp/Header/headers.tsx
@@ -1,7 +1,4 @@
 import {
-    createStyles,
-    Menu,
-    Center,
     Header,
     Container,
     Group,
@@ -10,49 +7,12 @@ import {
   } from '@mantine/core';
   import { useDisclosure } from '@mantine/hooks';
   import {HeaderAction} from './header-links'
+  import useStyles from './header.styles'
+
 
 
   const HEADER_HEIGHT = 60;
   
-  const useStyles = createStyles((theme) => ({
-    inner: {
-      height: HEADER_HEIGHT,
-      display: 'flex',
-      justifyContent: 'space-between',
-      alignItems: 'center',
-    },
-  
-    links: {
-      [theme.fn.smallerThan('sm')]: {
-        display: 'none',
-      },
-    },
-  
-    burger: {
-      [theme.fn.largerThan('sm')]: {
-        display: 'none',
-      },
-    },
-  
-    link: {
-      display: 'block',
-      lineHeight: 1,
-      padding: '8px 12px',
-      borderRadius: theme.radius.sm,
-      textDecoration: 'none',
-      color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.colors.gray[7],
-      fontSize: theme.fontSizes.sm,
-      fontWeight: 500,
-  
-      '&:hover': {
-        backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
-      },
-    },
-  
-    linkLabel: {
-      marginRight: 5,
-    },
-  }));
   
   export function Headings() {
     const { classes } = useStyles();
