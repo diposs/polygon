cd477dda7fa9ceb80c7a6169bb882494eba349af
Mogakamo <mogaka.amo254@gmail.com>
2022-08-29 22:36:41 +0300

    fix: revert about page to initial content

--- pages/About.tsx
+++ pages/About.tsx
@@ -1,16 +1,12 @@
 import Link from "next/link";
-import LoginButton from "../Comp/buttons";
 import Navbar from "../Comp/Navbar";
 
 const About = () => {
     return (
-        <>
                 <div className= "Check">
-                    <LoginButton />
-                    {/* <h1> Always loof</h1>
-                    <Navbar /> */}
+                    <h1> Always loof</h1>
+                    <Navbar />
                 </div>
-            </>
             
     );
 }
